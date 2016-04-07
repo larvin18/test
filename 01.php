64002cfe05658a1cea5e743dc815fca38f9e7215
yoshiki <helu3022@qq.com>
2016-04-07 17:08:24 +0800

    add 01.php
    新增php文件

---
+++ 01.php
@@ -0,0 +1,29 @@
+
+<?php
+/**
+
+����һ��PHP�ļ�
+
+*/
+/**
+ * Created by PhpStorm.
+ * Time: 10:59
+ */
+
+namespace Admin\Controller;
+
+
+
+class TakeGoldCoinController extends CommonAuthController
+{
+  
+
+    public function add(){
+
+
+        $this->display();
+    }
+
+    
+
+}
